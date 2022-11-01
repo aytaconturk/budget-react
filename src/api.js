09a7b7493a975cafecc0a02edee4d40668a20bce
aytaconturk <aytconturk@gmail.com>
2022-11-02 01:12:05 +0300

    api portu 8181 olarak degistirildi

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const api = () => {
     return axios.create({
-        baseURL: "http://localhost:8282",
+        baseURL: "http://localhost:8181",
         headers: {
             "Content-type": "application/json",
             accept: "application/json"
