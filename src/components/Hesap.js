eb33d59b285639ec6b0497cc7c15db1a14a927ef
aytaconturk <aytconturk@gmail.com>
2022-11-08 16:11:48 +0300

    harcama ve gelir tutarları hesaplandı

--- src/components/Hesap.js
+++ src/components/Hesap.js
@@ -1,22 +1,58 @@
+import { useEffect, useState } from "react";
+import { api } from "../api";
+
 const Hesap = () => {
+
+    const [harcamaTutari, setHarcamaTutari] = useState(0);
+    const [gelirTutari, setGelirTutari] = useState(0);
+
+    const harcamaTutariniHesapla = (kullaniciId) => {
+        new Promise((resolve) => {
+          let url = "/harcamalar/harcama-tutarini-getir/" + kullaniciId;
+    
+          api()
+            .get(url)
+            .then((yanit) => {
+              setHarcamaTutari(yanit.data);
+            });
+        });
+      }
+
+      const gelirTutariniHesapla = (kullaniciId) => {
+        new Promise((resolve) => {
+          let url = "/harcamalar/gelir-tutarini-getir/" + kullaniciId;
+    
+          api()
+            .get(url)
+            .then((yanit) => {
+              setGelirTutari(yanit.data);
+            });
+        });
+      }
+    
+      useEffect(() => {
+        harcamaTutariniHesapla(1)
+        gelirTutariniHesapla(1)
+      }, []);
+
     return (
         <div className="balance">
             <div className="card border-green">
                 <div className="card-header border-green-light">
                     <span>Total Balance
                         <i className="fa fa-angle-down" aria-hidden="true"></i></span>
-                    <h4>2500</h4>
+                    <h4>{gelirTutari - harcamaTutari}</h4>
                 </div>
                 <div className="card-body">
                     <div className="row">
                         <div className="col-6">
                             <h5 className="card-title">Income </h5>
                             <span type="button" className="badge badge-success" data-toggle="modal"
-                                data-target="#incomeModal">400$</span>
+                                data-target="#incomeModal">{gelirTutari}$</span>
                         </div>
                         <div className="col-6">
                             <h5 className="card-title">Expense</h5>
-                            <span className="badge badge-danger">1400$</span>
+                            <span className="badge badge-danger">{harcamaTutari}$</span>
                         </div>
                     </div>
                 </div>
