947c4e1ce768e267aed29a782b11873c39521cc1
aytaconturk <aytconturk@gmail.com>
2022-11-25 10:58:21 +0300

    api url ubuntu server ipsi ile değiştirildi

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const api = () => {
     return axios.create({
-        baseURL: "http://localhost:8282",
+        baseURL: "http://89.252.140.243:8080/",
         headers: {
             "Content-type": "application/json",
             accept: "application/json"
